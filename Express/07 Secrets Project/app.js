407636aa1951e52ccd24aa014f5345fcf10fb565
Murli Kumar <aimgodhere@gmail.com>
2026-06-30 14:34:00 +0530

    secrets project implemented where if pass is correct then he will redirect to the secret

---
+++ Express/07 Secrets Project/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.post("/check", (req, res) => {
+  const pass = req.body["password"];
+  if (pass !== "ILoveProgramming") {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  }
+  res.sendFile(path.join(__dirname, "public", "secrets.html"));
+});
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
